7f910d57d93eaadd49916304a85ac7a334461a46
MJ Gillado <mjgillado0@gmail.com>
2024-01-26 14:31:37 +0800

    Added program statement

---
+++ income_tax.py
@@ -0,0 +1,4 @@
+#  Calculate income tax for the given income by adhering to the rules below
+# Expected Output:
+# For example, suppose the taxable income is 45000, and the income tax payable is
+# 10000*0% + 10000*10%  + 25000*20% = $6000.
